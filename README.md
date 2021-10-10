315c9b4d32d1a0ab8629e65ba6f7b8ca78e2b086
ariadne-pereira <ariadnepereira98@gmail.com>
2021-10-09 22:03:43 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# unit-tests-beer-stock
+ Project to build unit tests to validate a beer inventory management REST API developed in Spring Boot.
---
+++ unitTestsBeerStock/src/test/java/one/digitalinnovation/unitTestsBeerStock/UnitTestsBeerStockApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.unitTestsBeerStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestsBeerStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
